a20b32ce03e395f6fc45f7049360bc5e965ed889
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-06 09:51:06 +0300

    Editing the input field

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -57,7 +57,7 @@ const demo = () => {
           <img src={linkIcon} alt="link"
             className="absolute left-0 my-2 ml-3 w-5"
           />
-          <input type="url" placeholder="Enter a Url"
+          <input type="url" placeholder="Enter a Url for the article"
             value={article.url}
             onChange={(e) => setArticle({
               ...article,
@@ -73,7 +73,6 @@ const demo = () => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
-
           </button>
         </form>
 
