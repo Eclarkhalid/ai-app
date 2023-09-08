0bde72d8df33c6f7964bd595499e49541431541e
Eclar Khalid <124664156+Eclarkhalid@users.noreply.github.com>
2023-09-08 16:37:43 +0300

    Update demo.jsx

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -70,9 +70,10 @@ const demo = () => {
           <button type="submit" className="submit_btn peer-focus:border-gray-700
           peer-focus:text-gray-700
           ">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
+            
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+</svg>
           </button>
         </form>
 
